b9994f1e6a30d865a3c88c904a666f835fe8944d
Swan Htet Thurein <66543551+SwanHtetThurein@users.noreply.github.com>
2024-01-08 22:32:06 -0500

    scrape.py

--- scrape.py
+++ scrape.py
@@ -3,19 +3,26 @@ import lxml.html
 
 html = requests.get('https://store.steampowered.com/explore/new/')
 doc = lxml.html.fromstring(html.content)
+
+#Extract the divs that have an id of "tab_newreleases_content" from the explore/new page 
 new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
 
-#Extracting Titles and Prices 
+#Extract Titles(item_name) and Price(discount_final_price) under new_releases  
 titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
 prices = new_releases.xpath('.//div[@class="discount_final_price"]/text()')
 
-tags = []
-for tag in new_releases.xpath('.//div[@class="tab_item_top_tags"]'):
-    tags.append(tag.text_content())
 
-tags = [tag.split(', ') for tag in tags]
+tags = []#Declare a list of tags 
+#Iterate over the list of extracted tags 
+for tag in new_releases.xpath('.//div[@class="tab_item_top_tags"]'):
+    tags.append(tag.text_content())#Extract the text from the tags using text_content method 
+
+tags = [tag.split(', ') for tag in tags]#Separate each tag with ',' so that each tag is a separate element 
+
+#Extract item_details under new_releases 
 platforms_div = new_releases.xpath('.//div[@class="tab_item_details"]')
 total_platforms = []
+
 
 for game in platforms_div:
     temp = game.xpath('.//span[contains(@class, "platform_img")]')
